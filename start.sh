e7cb052e59bdb42f6c6c004bf544dc76f9bdeba0
savezmalik786 <98945371+savezmalik786@users.noreply.github.com>
2022-03-10 08:04:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/savezmalik786/filefilterbo7.git /savezmalik786
+  git clone https://github.com/savezmalik786/filefilterbo7.git /EvaMaria elce
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /filefilterbo7
 fi
